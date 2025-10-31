7a120328fab559c66c368ba189b04d633b44d8e4
balaraju <balaraju@bijib.in>
2025-10-31 15:41:48 +0530

    changes

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -6,7 +6,8 @@ import { Observable } from "rxjs"
 })
 export class Constants {
 
- static readonly API_URL: string = "http://localhost:3000/"
+ // static readonly API_URL: string = "http://localhost:3000/"
+ static readonly API_URL: string = "https://portfolio-node-by-balaraju.vercel.app/"
 
  static readonly GET_PORTFOLIO_URL = `${Constants.API_URL}portfolio/get`
 }
